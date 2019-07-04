f63fee51905a70364a0f37721d426b440509f755
Matthias Held <ilovemilk@wusa.io>
2019-07-04 21:11:08 +0200

    add implementation for PUT,DELTE,POST with curl

--- lib/RequestTemplate.php
+++ lib/RequestTemplate.php
@@ -32,11 +32,44 @@ class RequestTemplate implements IRequestTemplate {
     }
 
     public function post($url, $queryData) {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $queryData);                                                                  
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+            'Content-Type: application/json',                                                                                
+            'Content-Length: ' . strlen($data_string))                                                                       
+        );                                                                                                                   
+                                                                                                                            
+        $result = curl_exec($ch);
+        curl_close($ch);    
+        return $result;
     }
 
     public function put($url, $queryData) {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");                                                                     
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $queryData);                                                                  
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     
+                                                                         
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+            'Content-Type: application/json',                                                                                
+            'Content-Length: ' . strlen($data_string))                                                                       
+        );                                                                                                                   
+                                                                                                                            
+        $result = curl_exec($ch);
+        curl_close($ch);    
+        return $result;
     }
 
     public function delete($url) {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");  
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);   
+        $result = curl_exec($ch);
+        curl_close($ch);    
+        return $result;
     }
 }
